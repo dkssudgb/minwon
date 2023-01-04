3d111a9951cb01a094945bd402cd08f5685f8e68
dkssudgb <leehb9360@likelion.org>
2023-01-04 21:28:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 # from streamlit_option_menu import option_menu
+from PIL import Image
+# import pandas as pd
+# from PyPDF2 import PdfFileReader
+# import os
 
 st.set_page_config(
     page_title="국민신문고 민원 분류 자동화 ",
@@ -8,8 +12,16 @@ st.set_page_config(
 )
 
 st.title("국민신문고 민원 담당 기관 분류 예측")
-
+st.header("")
+st.subheader("")
 st.markdown("# ")
+
+def load_image(img_file):
+    img = Image.open(img_file)
+    return img
+
+img = load_image("./images/news.png")
+st.image(img)
 
 # with st.sidebar:
 #     selected = option_menu("민원데이터 분류", ["홈", "모델 예측 결과", "모델 설명", "시각화"], 
