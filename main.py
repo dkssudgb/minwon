e13b6e82d77a1ebe09dcf93c70979131e5ca22a6
dkssudgb <leehb9360@likelion.org>
2023-01-04 21:19:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.set_page_config(
 st.markdown("# EDA")
 st.markdown("## 주제 : 국민신문고 민원 분류 자동화 ")
 
-with st.sidebar:
-    selected = option_menu("민원데이터 분류", ["홈", "모델 예측 결과", "모델 설명", "시각화"], 
-        icons=['home', 'search', "explain", "chart"], menu_icon="menu", default_index=1)
-    selected
+# with st.sidebar:
+#     selected = option_menu("민원데이터 분류", ["홈", "모델 예측 결과", "모델 설명", "시각화"], 
+#         icons=['home', 'search', "explain", "chart"], menu_icon="menu", default_index=1)
+#     selected
